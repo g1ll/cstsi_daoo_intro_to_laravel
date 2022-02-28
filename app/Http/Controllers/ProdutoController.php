06521ca54cc25fca106364db198f295643cb336b
g1ll from d3567 <gillvelleda@gmail.com>
2022-02-28 15:03:21 -0300

    Refactoring: simplify method index.

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -3,20 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Produto;
-use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
 {
 
-    private $produto;
-
     public function index(){
-        $this->produto = new Produto();
-        // return response()->json($this->produto->all());
-        return view('produtos',['produtos'=>$this->produto->all()]);
+        return view('produtos',['produtos'=>Produto::all()]);
     }
-
-    //show(id)->produto->find(id)
-
 
 }
