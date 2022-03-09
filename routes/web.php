9833bc8dc3f767ad85908e8f4da8a142939c735f
g1ll from d3567 <gillvelleda@gmail.com>
2022-03-09 09:31:08 -0300

    Create CRUD Routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use  App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\HomeController;
+use App\Models\Produto;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,5 +25,23 @@ Route::get('/', function () {
 // });
 
 Route::get('/ola',[HomeController::class,'index']);
-Route::get('/produtos',[ProdutoController::class,'index']);
+Route::get('/produtos',[ProdutoController::class,'index'])->name('index');
+Route::get('/produto/{id}',[ProdutoController::class,'show'])->name('show');
+
+//Create
+Route::get('/produto',[ProdutoController::class,'create']);
+Route::post('/produto',[ProdutoController::class,'store']);
+
+// Update
+Route::get('/produto/{id}/edit',[ProdutoController::class,'edit'])->name('edit');
+Route::post('/produto/{id}/update',[ProdutoController::class,'update'])->name('update');
+
+// Route::get('/produto/{id}/edit',function($id){
+//     return view('produto_edit',['produto'=>Produto::find($id)]);
+// })->name('edit');
+
+//Delete
+Route::get('/produto/{id}/delete',[ProdutoController::class,'delete'])->name('delete');
+Route::post('/produto/{id}/remove',[ProdutoController::class,'remove'])->name('removeProduto');
+
 
