49c2253cb00d04e405561a3b9e10f7933ecd53d4
g1ll from d3567 <gillvelleda@gmail.com>
2022-03-09 09:29:19 -0300

    Create fillalble attributes

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Produto extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'nome',
+        'descricao',
+        'qtd_estoque',
+        'preco',
+        'importado'
+    ];
+    
 }
