d52ea3aa08d487b2bda448f8881b9622315a5fca
g1ll from d3567 <gillvelleda@gmail.com>
2022-04-06 16:28:32 -0300

    Atualização do método login e criação do método logout.

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
 use App\Models\User;
 use Exception;
 use Illuminate\Support\Facades\Hash;
@@ -16,7 +17,10 @@ class LoginController extends Controller
             $user = User::where('email',$request->email)->first();
             if(!$user || !Hash::check($request->password,$user->password))
                 throw new Exception('Senha incorreta!!');
-            $token = $user->createToken($request->email)->plainTextToken;
+            
+            $ability = $user->is_admin?['is-admin']:[];
+            $token = $user->createToken($request->email,$ability)->plainTextToken;
+            
             return response()->json(['token'=>$token]);
         }catch(Exception $error){
             return response()->json([
@@ -28,5 +32,18 @@ class LoginController extends Controller
     public function user()
     {
         return auth()->user();
+    }
+
+    public function logout(Request $request)
+    {
+        $auth_user = $request->user();
+        if($request->input('all')){
+            if($auth_user->tokens()->delete())
+                return ['logout'=>'Desconectado de todos os dispositivos!'];
+        }else{
+            if($auth_user->currentAccesstoken()->delete())
+                return ['logout'=>'Usuário desconectado!'];
+        }
+        return response()->json(['logout'=>'Falha ao desconectar Usuário!'],500);
     }
 }
