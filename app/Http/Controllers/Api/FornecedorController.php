b0d63c05cf671edcc7e285eb5e650e0e07cab650
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-04-20 18:52:26 -0300

    Exemplo de uso do FormRequest com Fornecedores: exemplo de revisão

--- app/Http/Controllers/Api/FornecedorController.php
+++ app/Http/Controllers/Api/FornecedorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\FornecedorRequest;
 use App\Models\Fornecedor;
 use Exception;
 use Illuminate\Http\Request;
@@ -32,7 +33,7 @@ class FornecedorController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(FornecedorRequest $request)
     {
         $statusHttp = 500;
         try{
