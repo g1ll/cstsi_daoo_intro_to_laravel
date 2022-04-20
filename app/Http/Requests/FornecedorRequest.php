bca3121ff3fce654f172c66d4936de5ac5a1c923
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-04-20 18:57:25 -0300

    Criação do FornecedorRequest para aula de revisão. >php artisan make:request FornecedorRequest

---
+++ app/Http/Requests/FornecedorRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class FornecedorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'cnpj'  =>  'required'
+        ];
+    }
+
+    public function messages(){
+        return [
+            'cnpj.required' =>  'O CNPJ é Obrigatório!!!'
+        ];
+    }
+}
